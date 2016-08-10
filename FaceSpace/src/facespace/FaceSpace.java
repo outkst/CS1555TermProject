2dbeb3f7f09dbd3d6429c7b89b8e734075066208
Joe Meszar <github@outk.st>
2016-08-09 21:09:57 -0400

    Formatted the FaceSpace code structure
    - Changed around the user input screen options.
    
    - Added a catch for sqlException from the user input code block.
    
    - printSQLException() method made safer by checking for null.
    
    - getUserString() method fixed by removing unnecessary checks that were
    	in place from the getUserEmail() method copy.

--- FaceSpace/src/facespace/FaceSpace.java
+++ FaceSpace/src/facespace/FaceSpace.java
@@ -3,7 +3,6 @@ package facespace;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
-import oracle.jdbc.driver.DatabaseError;
 
 /**
  *
@@ -22,7 +21,7 @@ public class FaceSpace {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to FaceSpaces!");
         DatabaseConnection db = null;
-        String url, username, password;
+        String url = "", username = "", password = "";
         
         try {
             // start by getting the username and password
@@ -31,19 +30,21 @@ public class FaceSpace {
             
             // get the database url (default to Pitt db if blank)
             System.out.print("Please enter the URL of the database (leave blank to use Pitt's db): ");
-            url = keyIn.nextLine().toLowerCase();
+            //url = keyIn.nextLine().toLowerCase();
             if (url.equals("")) { url = "jdbc:oracle:thin:@class3.cs.pitt.edu:1521:dbclass"; }
             
-            // DEBUG URL
+            // DEBUG INFO
             url = "jdbc:oracle:thin:@localhost:1521:xe";
+            username = "system";
+            password = "poiu0987";
             
             // get the username
-            System.out.print("Please enter the DB username: ");
-            username = keyIn.nextLine();
+            //System.out.print("Please enter the DB username: ");
+            //username = keyIn.nextLine();
             
             // get the password
-            System.out.print("Please enter the DB password: ");
-            password = keyIn.nextLine();
+            //System.out.print("Please enter the DB password: ");
+            //password = keyIn.nextLine();
             
             // create the database connection instance.
             try {
@@ -56,42 +57,21 @@ public class FaceSpace {
             int input = -1;
             while (input != 0) {
                 // show menu and get user input
-                System.out.print("\nWhat would you like to do? (Enter 0 to exit)\n"
-                        + "1 - Create a User \t\t\t 8 - Send Group a Message \t\t\t 15 - Log in User \n"
-                        + "2 - Initiate a friendship \t\t 9 - Display Messages for User \t\t\t 16 - List All Users \n"
-                        + "3 - Establish a friendship \t\t 10 - Display New Messages for User \t\t 17 - List All Groups \n"
-                        + "4 - Display User's friends \t\t 11 - Search for User\n"
-                        + "5 - Create a Group \t\t\t 12 - Three Degrees Between Users\n"
-                        + "6 - Add User to Group \t\t\t 13 - Top Messagers\n"
-                        + "7 - Send User a Message \t\t 14 - Drop a User\n"
+                System.out.print("\n\nWHAT WOULD YOU LIKE TO DO? (ENTER 0 TO EXIT)\n\n"
+                        + "1 - ADD TO GROUP\t\t7 - DROP USER\t\t\t13 - SEARCH FOR USER\n"
+                        + "2 - CREATE GROUP\t\t8 - ESTABLISH FRIENDSHIP\t14 - SEND MESSAGE TO GROUP\n"
+                        + "3 - CREATE USER\t\t\t9 - INITIATE FRIENDSHIP\t\t15 - SEND MESSAGE TO USER\n"
+                        + "4 - DISPLAY FRIENDS\t\t10 - LIST ALL GROUPS\t\t16 - THREE DEGREES\n"
+                        + "5 - DISPLAY MESSAGES\t\t11 - LIST ALL USERS\t\t17 - TOP MESSAGERS\n"
+                        + "6 - DISPLAY NEW MESSAGES\t12 - LOG IN USER\n"
                         + "\nEnter command: ");
                 input = tryParseInt(keyIn.nextLine()); // returns -1 if bad input
                 
                 switch (input) {
-                    case 0: System.out.println("\nGoodbye!");
+                    case 0: System.out.println("\nGOODBYE!");
                         break;
                         
-                    case 1: System.out.println("\n[CREATE USER]");
-                        db.createUser();
-                        break;
-                        
-                    case 2: System.out.println("\n[INITIATE FRIENDSHIP]");
-                        db.initiateFriendship();
-                        break;
-                        
-                    case 3: System.out.println("\n[ESTABLISH FRIENDSHIP]");
-                        db.establishFriendship();
-                        break;
-                        
-                    case 4: System.out.println("\n[DISPLAY FRIENDS]");
-                        db.displayFriends();
-                        break;
-                        
-                    case 5: System.out.println("\n[CREATE GROUP]");
-                        db.createGroup();
-                        break;
-                        
-                    case 6: System.out.println("\n[ADD TO GROUP]");
+                    case 1: System.out.println("\n[ADD TO GROUP]");
                         try {
                             String userEmail = getUserEmail("Please enter the user's email address: ");
                             String groupName = getUserString("Please enter the name of the group to join: ");
@@ -106,56 +86,76 @@ public class FaceSpace {
                         }
                         break;
                         
-                    case 7: System.out.println("\n[SEND MESSAGE TO USER]");
-                        db.sendMessageToUser();
+                    case 2: System.out.println("\n[CREATE GROUP]");
+                        db.createGroup();
                         break;
                         
-                    case 8: System.out.println("\n[SEND MESSAGE TO GROUP]");
-                        db.sendMessageToGroup();
+                    case 3: System.out.println("\n[CREATE USER]");
+                        db.createUser();
                         break;
                         
-                    case 9: System.out.println("\n[DISPLAY MESSAGES]");
+                    case 4: System.out.println("\n[DISPLAY FRIENDS]");
+                        db.displayFriends();
+                        break;
+                        
+                    case 5: System.out.println("\n[DISPLAY MESSAGES]");
                         db.displayMessages();
                         break;
                         
-                    case 10: System.out.println("\n[DISPLAY NEW MESSAGES]");
+                    case 6: System.out.println("\n[DISPLAY NEW MESSAGES]");
                         db.displayNewMessages();
                         break;
                         
-                    case 11: System.out.println("\n[SEARCH FOR USER]");
-                        db.searchForUser();
-                        break;
-                        
-                    case 12: System.out.println("\n[THREE DEGREES]");
-                        db.threeDegrees();
-                        break;
-                        
-                    case 13: System.out.println("\n[TOP MESSAGERS]");
-                        db.topMessagers();
-                        break;
-                        
-                    case 14: System.out.println("\n[DROP USER]");
+                    case 7: System.out.println("\n[DROP USER]");
                         db.dropUser();
                         break;
                         
-                    case 15: System.out.println("\n[LOG IN USER]");
-                        db.logInUser();
+                    case 8: System.out.println("\n[ESTABLISH FRIENDSHIP]");
+                        db.establishFriendship();
+                        break;
+                    
+                    case 9: System.out.println("\n[INITIATE FRIENDSHIP]");
+                        db.initiateFriendship();
                         break;
                         
-                    case 16: System.out.println("\n[LIST ALL USERS]");
+                    case 10: System.out.println("\n[LIST ALL GROUPS]");
+                        db.listAllGroups();
+                        break;
+                        
+                    case 11: System.out.println("\n[LIST ALL USERS]");
                         db.listAllUsers();
                         break;
                         
-                    case 17: System.out.println("\n[LIST ALL GROUPS]");
-                        db.listAllGroups();
+                    case 12: System.out.println("\n[LOG IN USER]");
+                        db.logInUser();
+                        break;
+                        
+                    case 13: System.out.println("\n[SEARCH FOR USER]");
+                        db.searchForUser();
+                        break;
+                        
+                    case 14: System.out.println("\n[SEND MESSAGE TO GROUP]");
+                        db.sendMessageToGroup();
+                        break;
+                        
+                    case 15: System.out.println("\n[SEND MESSAGE TO USER]");
+                        db.sendMessageToUser();
+                        break;
+                        
+                    case 16: System.out.println("\n[THREE DEGREES]");
+                        db.threeDegrees();
+                        break;
+                        
+                    case 17: System.out.println("\n[TOP MESSAGERS]");
+                        db.topMessagers();
                         break;
                         
                     default: System.out.println("INVALID INPUT");
                 }
             }
-//        } catch (SQLException e) {
-//            System.out.println(String.format("\n!! SQL Error: %s", e.getMessage()));
-//            
+        } catch (SQLException e) {
+            printSQLException(e);
+            
         } catch (Exception e) {
             System.out.println(String.format("\n!! Error: %s", e.getMessage()));
             
@@ -172,18 +172,24 @@ public class FaceSpace {
     public static void printSQLException(SQLException e) {
         try {
             while (e != null) {
+                int errorCode = e.getErrorCode();
+                String sqlState = e.getSQLState();
+                String errorMessage = e.getMessage();
+                
                 // Gets the error number associated with the exception.
-                System.err.println("ERROR CODE: " + e.getErrorCode());
+                System.err.println("ERROR CODE: " + errorCode);
                 
                 // Gets the XOPEN SQLstate string. For a JDBC driver error, 
                 //  no useful information is returned from this method. For 
                 //  a database error, the five-digit XOPEN SQLstate code is 
                 //  returned. This method can return null.
-                System.err.println("SQL STATE: " + e.getSQLState().trim().replaceAll("\n", " "));
+                if (sqlState == null || sqlState.isEmpty()) { sqlState = "(empty)"; }
+                System.err.println("SQL STATE: " + sqlState.trim().replaceAll("\n", " "));
                 
                 // Gets the JDBC driver's error message for an error, handled by the driver
                 //  or gets the Oracle error number and message for a database error.
-                System.err.println("SQL ERROR MESSAGE: " + e.getMessage().trim().replaceAll("\n", " "));
+                if (errorMessage == null || errorMessage.isEmpty()) { errorMessage = "(empty)"; }
+                System.err.println("SQL ERROR MESSAGE: " + errorMessage.trim().replaceAll("\n", " "));
 
                 //Gets the next Exception object in the exception chain.
                 e = e.getNextException();
@@ -195,7 +201,8 @@ public class FaceSpace {
     
     
     /**
-     * Gets a string from the user input.
+     * Gets a string from the user input. Normalized to uppercase.
+     * 
      * @param message The message to display to the user before getting the input.
      * @return A non-null, non-empty String value (no leading/trailing spaces).
      */
@@ -203,14 +210,15 @@ public class FaceSpace {
         String userString;
         do {
             System.out.print(message);
-            userString = keyIn.nextLine().trim().toLowerCase();
-        } while (userString == null || userString.equalsIgnoreCase("") || !Pattern.matches("^([a-zA-Z0-9]+([\\.+_-][a-zA-Z0-9]+)*)@(([a-zA-Z0-9]+((\\.|[-]{1,2})[a-zA-Z0-9]+)*)\\.[a-zA-Z]{2,6})$", userString));
+            userString = keyIn.nextLine().trim().toUpperCase();
+        } while (userString == null || userString.equalsIgnoreCase(""));
         
         return userString;
     }
     
     /**
      * Gets a number from the user input.
+     * 
      * @param message The message to display to the user before getting the input.
      * @return A valid integer value.
      */
@@ -225,7 +233,8 @@ public class FaceSpace {
     }
     
     /**
-     * Gets a valid email address from the user input.
+     * Gets a valid email address from the user input. Normalized to lowercase.
+     * 
      * @param message The message to display to the user before getting the input.
      * @return A valid, normalized, email address (lowercase, no leading/trailing spaces).
      */
@@ -241,6 +250,7 @@ public class FaceSpace {
     
     /**
      * Parses an integer value from the given String.
+     * 
      * @param value A string value from which to get an integer value.
      * @return The value of the parsed integer from the string; otherwise -1.
      */
@@ -248,7 +258,7 @@ public class FaceSpace {
         int num;
         try {  
             num = Integer.parseInt(value);  
-            return num;  
+            return num;
          } catch (NumberFormatException e) {  
             return -1;  
          }  
